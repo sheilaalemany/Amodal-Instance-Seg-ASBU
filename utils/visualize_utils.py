c853fcb2222017bd18fcf689724b0ea2808a9533
sheilaalemany <salem010@fiu.edu>
2023-07-11 16:30:27 -0400

    will this work?

--- utils/visualize_utils.py
+++ utils/visualize_utils.py
@@ -25,9 +25,12 @@ def visualize_tensor(tensors_dict, mean, div):
         
         # ot = tensors_dict['originals'].detach().cpu()
         ot = tensors_dict['originals']
+        ot = torch.tensor(ot)
+        print('old ot shape: ', ot.shape)
+        ot = ot.reshape(ot, (32, 3, 256, 256))
         # the shape of original images is such that it is 32 separate pics but I want one pic with the 32 lil guys
         # ot = torch.reshape(ot, (32, 3, 256, 256))
-        print('ot shape: ', ot.shape)
+        print('new ot shape: ', ot.shape)
         together.append(ot)
         
         # for ot in tensors_dict['originals']:
