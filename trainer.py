58c6f73e03b40b00b321508ec6e0dd913c61cdfb
sheilaalemany <salem010@fiu.edu>
2023-07-14 14:31:12 -0400

    finding where the annotation filenames are

--- trainer.py
+++ trainer.py
@@ -247,7 +247,8 @@ class Trainer(object):
         with open("images_used_for_masks.json", "w") as outfile:
             for j in range(len(images_info_for_filenames[0])): # it seems every index has the same group of images indexed
                 img_info = images_info_for_filenames[j]
-                json.dump(img_info['file_name']+'\n', outfile) 
+                json.dump(img_info['file_name'], outfile) 
+                outfile.write('\n')
                 print(img_info['file_name'])
         
         all_together = []
