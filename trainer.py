5e30f4814c41203e188462956aaeec35e6e473bf
sheilaalemany <salem010@fiu.edu>
2023-07-21 14:05:18 -0400

    trying to extract more masks

--- trainer.py
+++ trainer.py
@@ -218,8 +218,9 @@ class Trainer(object):
             print('len(images_info_for_filenames): ', len(images_info_for_filenames))
             for b in range(len(images_info_for_filenames)):
                 print('len(images_info_for_filenames[b]): ', len(images_info_for_filenames[b]))
+                batch_img = images_info_for_filenames[b]
                 for j in range(len(images_info_for_filenames[b])): # it seems every index has the same group of images indexed
-                    img_info = images_info_for_filenames[b][j]
+                    img_info = batch_img[j]
                     json.dump(img_info['file_name'], outfile) 
                     outfile.write('\n')
             print('...image filenames of the batch corresponding to masks saved in file batch_images_used_for_masks.json')
