e524a589f00e38371e9f574492d708efdf0463fc
sheilaalemany <salem010@fiu.edu>
2023-07-25 18:01:50 -0400

    images we got were out of order so trying to fix

--- trainer.py
+++ trainer.py
@@ -109,6 +109,7 @@ class Trainer(object):
                                            sampler=train_sampler)
 
         # args.data.val_image_root = 'data/COCOA/val2014' # originally
+        print(args.data)
         args.data.val_image_root = '/aul/homes/byang010/attacking-amodal/COCOA/s_val2014/animals'
         
         val_dataset = trainval_class(args.data, 'val')
